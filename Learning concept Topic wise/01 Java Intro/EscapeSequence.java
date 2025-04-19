f2abd74d2ef78369d47354cfede0625992187e21
Pankaj <pk9278424@gmail.com>
2025-04-19 23:06:33 +0530

    UPDATE

---
+++ Learning concept Topic wise/01 Java Intro/EscapeSequence.java
@@ -0,0 +1,34 @@
+public class EscapeSequence 
+{
+    public static void main(String[] args) 
+    {
+        // System.out.println("Hello World 1");
+        // System.out.println("Hello World 2");
+        // //here ln means new line (\n) in the end of msg
+
+        // System.out.println(); //empty println means \n
+
+        // System.out.print("Hello");
+        // System.out.print("Pankaj");
+        // System.out.print("Sir");
+
+        // System.out.print("\nHello\n");
+        // System.out.print("Pankaj\n");
+        // System.out.print("Sir\n");
+
+        // System.out.print("Hello\t");
+        // System.out.print("Pankaj\t");
+        // System.out.print("Sir\n");
+
+        // System.out.printf("Hii i am %d printf \n",56);
+        // System.out.println("Hello 'Pankaj' Tiwari ");
+        // System.out.println("Hello "Pankaj" Tiwari ");//error
+        // System.out.println(" it is \"Pankaj\" Home");
+        // System.out.println(" it is \" Pankaj's \" Home");
+        // System.out.println(" \ "); //error
+        // System.out.println(" \\ ");
+        System.out.println("Pankaj\b ");//after every \b space or any character  is required
+        // System.out.println("Pankaj\b.");//after every \b space or any character is required
+        // System.out.println("Pankaj\b \b ");//after every \b space or any character  is required
+    }
+}
