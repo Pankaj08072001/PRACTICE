71430b0e0f336182cfbbab5ffc1d2da80dd4e2b1
Pankaj <pk9278424@gmail.com>
2025-05-05 21:48:21 +0530

    Update

--- ArrayQuestion/Test0.java
+++ ArrayQuestion/Test0.java
@@ -5,14 +5,14 @@ import java.util.Scanner;
 public class Test0 {
 	public static void main(String[] args) throws Throwable  {
 		Scanner sc = new Scanner(System.in);
-		int length = sc.nextInt();
-		int breath = sc.nextInt();
-		if (length < 0 || breath < 0) {
-			 //throw new Exception("");
-		} else {
-			int area = length * breath;
-			System.out.println(area);
+		int size = sc.nextInt();
+		for (int i = 0; i < size; i++) {
+			for (int j = size; j >=i; j--) {
+				System.out.print(j-i);
+			}
+			System.out.println();
 		}
 		sc.close();
+		
 	}
 }
