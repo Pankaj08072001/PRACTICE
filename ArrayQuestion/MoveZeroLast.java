d0489e3fddc9d6121dcb1d3587e80e395739a0a6
Pankaj <pk9278424@gmail.com>
2025-05-05 22:28:11 +0530

    Code

--- ArrayQuestion/MoveZeroLast.java
+++ ArrayQuestion/MoveZeroLast.java
@@ -4,12 +4,12 @@ import java.util.Arrays;
 
 public class MoveZeroLast {
 	public static void main(String[] args) {
-		int[] arr = { 0, 1, 2, 1, 0, 2, 1, 0, 2 };
+		int[] arr = { 0, 1, 2, 1, 0, 2, 1, 0, 2,0,0,0,0 };
 		int b = arr.length - 1;
 
 		for (int i = 0; i < b; i++) {
 			if (arr[i] == 0) {
-				while (b > i && arr[b] == 0) {
+				while (arr[b] == 0) {
 					b--;
 			}
 				int temp = arr[b];
