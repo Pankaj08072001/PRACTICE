a2d9e577a0d73791da670fe6d73f22c39c78193c
Pankaj <pk9278424@gmail.com>
2025-06-14 14:26:29 +0530

    update

--- InterviewQuestion/Removeduplicate.java
+++ InterviewQuestion/Removeduplicate.java
@@ -22,7 +22,7 @@ public class Removeduplicate {
 		Arrays.sort(arr);
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
-				if (arr[i] == arr[j] && i != j) {
+				if (arr[i] == arr[j] && i!=j) {
 					System.out.print(arr[j] + " ");
 					break;
 				}
