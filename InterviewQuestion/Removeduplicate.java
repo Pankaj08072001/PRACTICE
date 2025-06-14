ec19411c3828d5e67f3f2b7ecb56b11b842a985b
Pankaj <pk9278424@gmail.com>
2025-06-14 12:35:33 +0530

    update

---
+++ InterviewQuestion/Removeduplicate.java
@@ -0,0 +1,25 @@
+package InterviewQuestion;
+
+import java.util.Arrays;
+
+public class Removeduplicate {
+	public static void main(String[] args) {
+		//int arr[] = { 10, 15,5, 20, 10, 15, 20 ,2,2,2};
+		// Remove Dupicate
+		// Arrays.sort(arr);
+		// int j = 0;
+		// for (int i = 0; i < arr.length; i++) {
+		// 	if(arr[i] != arr[j]){
+		// 		j++;
+		// 		arr[j] = arr[i];
+		// 	} 	
+		// }
+		// System.out.println(Arrays.toString(Arrays.copyOf(arr, j+1)));
+
+
+		// Repeated elements
+
+		int arr[] = { 10, 15,5, 20, 10, 15, 20 ,2,2,2};
+		
+	}
+}
