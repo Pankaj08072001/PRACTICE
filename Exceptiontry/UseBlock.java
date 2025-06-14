cea357d94f17191944c3a505f6a23767df9d8ca8
Pankaj <pk9278424@gmail.com>
2025-06-14 10:26:05 +0530

    update

--- Exceptiontry/UseBlock.java
+++ Exceptiontry/UseBlock.java
@@ -4,15 +4,32 @@ import java.util.Scanner;
 
 public class UseBlock {
 	public static void main(String[] args) {
-		try (Scanner sc = new Scanner(System.in)) {
-			System.out.println("Enter the number a and b : ");
-			int a = sc.nextInt();
-			int b = sc.nextInt();
+		// try (Scanner sc = new Scanner(System.in)) {
+		// System.out.println("Enter the number a and b : ");
+		// int a = sc.nextInt();
+		// int b = sc.nextInt();
 
-			try {
-				System.out.println(a/b);
-			} catch (Exception e) {
-				System.out.println(e);
+		// try {
+		// System.out.println(a/b);
+		// } catch (Exception e) {
+		// System.out.println(e);
+		// }
+		// }
+
+		String str = "pankajr8448297";
+		String letter = "";
+		String digit = "";
+
+		int a = str.length() - 1;
+
+		for (int i = 0; i < a; i++) {
+			char ch = str.charAt(i);
+			if (Character.isDigit(ch)) {
+				digit += ch;
+			}
+
+			else {
+				letter += ch;
 			}
 		}
 	}
---
+++ InterviewQuestion/Findelement.java
@@ -0,0 +1,25 @@
+package InterviewQuestion;
+
+import java.util.Scanner;
+
+public class Findelement {
+	public static void main(String[] args) {
+		int arr[] = {10,15,12,52,16,17,1,78};
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter your number you want search ");
+		int n = sc.nextInt();
+		int k =0;
+
+		for (int i : arr) {
+			if(i==n){
+				System.out.println("Element is :"+ i);
+				k++;
+				break;
+			}
+		}
+		if(k==0){
+			System.out.println("Element is not found ");
+		}
+	}
+
+}
