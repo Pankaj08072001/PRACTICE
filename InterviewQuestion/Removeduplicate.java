c11053c699d8fb731a0947f59f431e004e330bce
Pankaj <pk9278424@gmail.com>
2025-06-14 13:00:33 +0530

    update

--- InterviewQuestion/Removeduplicate.java
+++ InterviewQuestion/Removeduplicate.java
@@ -4,22 +4,29 @@ import java.util.Arrays;
 
 public class Removeduplicate {
 	public static void main(String[] args) {
-		//int arr[] = { 10, 15,5, 20, 10, 15, 20 ,2,2,2};
+		// int arr[] = { 10, 15,5, 20, 10, 15, 20 ,2,2,2};
 		// Remove Dupicate
 		// Arrays.sort(arr);
 		// int j = 0;
 		// for (int i = 0; i < arr.length; i++) {
-		// 	if(arr[i] != arr[j]){
-		// 		j++;
-		// 		arr[j] = arr[i];
-		// 	} 	
+		// if(arr[i] != arr[j]){
+		// j++;
+		// arr[j] = arr[i];
+		// }
 		// }
 		// System.out.println(Arrays.toString(Arrays.copyOf(arr, j+1)));
 
-
 		// Repeated elements
 
-		int arr[] = { 10, 15,5, 20, 10, 15, 20 ,2,2,2};
-		
+		int arr[] = { 10, 15, 5, 20, 10, 15, 20, 2, 2, 10 };
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (arr[i] == arr[j] && i != j) {
+					System.out.print(arr[j] + " ");
+					break;
+				}
+			}
+		}
 	}
 }
